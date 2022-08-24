ad12b6ac5b53ac0cb71f8f4c1e48906220b3b1ae
hoomzy <hoomzy@DESKTOP-BSPB1C9>
2022-08-24 20:27:34 +0900

    효주_react_header,footer,app수정_220824

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,7 +1,28 @@
 import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import MainImage from "./components/main/MainImage";
+import EventCard from "./components/main/EventCard";
+import MainShowroom from "./components/main/MainShowroom"
 
 function App() {
-  return;
+  return (
+    <div>
+      <MainImage/>
+
+      
+      <h4 className="main_text">주목할 만한 인테리어 소품</h4>
+
+      <h4 className="main_text">진행중인 이벤트 및 프로모션</h4>
+      
+      <EventCard />
+
+      <h4 className="main_text">다양한 HOME CONTROLLER 서비스</h4>
+
+      <h4 className="main_text">인테리어 쇼룸</h4>
+      <MainShowroom />
+      
+    </div>
+  )
 }
 
 export default App;
---
+++ src/main/frontend/src/components/main/MainShowroom.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import MainShowroomColor from '../../components/main/MainShowroomColor';
+
+
+const MainShowroom = () => {
+    return (
+        <div>
+            <MainShowroomColor color="red" />
+            <MainShowroomColor color="yellow" />
+            <MainShowroomColor color="green" />
+            <MainShowroomColor color="blue" />
+            <MainShowroomColor color="purple" />
+            <MainShowroomColor color="white" />
+            <MainShowroomColor color="black" />
+            <MainShowroomColor color="pink" />
+            <MainShowroomColor color="gray" />
+            <MainShowroomColor color="brown" />    
+        </div>
+    );
+};
+
+export default MainShowroom;
