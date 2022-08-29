8ee185497304d7719107503dbd10a5476ce20f12
tjdcks0875 <tnsqor0875@naver.com>
2022-08-29 20:04:49 +0900

    성찬_Admin USERMANAGE_20220829

--- src/main/java/com/bootreact/hmct/controller/admin/AdminController.java
+++ src/main/java/com/bootreact/hmct/controller/admin/AdminController.java
@@ -1,31 +1,155 @@
 package com.bootreact.hmct.controller.admin;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.bootreact.hmct.dto.ResponseDTO;
+import com.bootreact.hmct.dto.UserDTO;
 import com.bootreact.hmct.entity.User;
+import com.bootreact.hmct.service.user.UserService;
 
+@RestController
+@RequestMapping("/api/admin")
 public class AdminController {
+//    @Autowired
+//    AdminService adminService;
+    
+//    @Autowired
+//    UserService userService;
+//   
+//    //회원 정보 조회  
+//    @GetMapping("/selectUserList")
+//    public ResponseEntity<?> getUserList(@AuthenticationPrincipal String userName){
+//    	try {
+//    		List<User> userList = userService.getUserList(userName);
+//    		
+//    		List<UserDTO> userDTOList = new ArrayList<UserDTO>();
+//    		
+//    		for(User t: userList) {
+//    			UserDTO userDTO = new UserDTO();
+//    			userDTO.setUserId(t.getUserId());
+//    			userDTO.setUserName(t.getUserName());
+//    			userDTO.setUserNickname(t.getUserNickname());
+//    			userDTO.setUserTel(t.getUserTel());
+//    			userDTO.setUserMail(t.getUserMail());
+//    			userDTO.setUserRole(t.getUserRole());
+//    			userDTO.setUserZip(t.getUserZip());
+//    			userDTO.setUserAddr(t.getUserAddr());
+//    			userDTO.setUserAddrDetail(t.getUserAddrDetail());
+//    			userDTO.setUserPoint(t.getUserPoint());
+//    			userDTO.setUserMarketing(t.getUserMarketing());
+//    			
+//    			userDTOList.add(userDTO);	
+//    		}
+//    		ResponseDTO<UserDTO> response = new ResponseDTO<>();
+//    		
+//    		response.setData(userDTOList);
+//    		
+//    		return ResponseEntity.ok().body(response);
+//    		
+//    	}catch(Exception e){
+//    		System.out.println(e.getMessage());
+//    		ResponseDTO<UserDTO> response = new ResponseDTO<>();
+//    		response.setError(e.getMessage());
+//    		return ResponseEntity.badRequest().body(response);		
+//    	}
+//    };
+//    //회원 삭제
+//    @DeleteMapping("/deleteAdminUser")
+//    public ResponseEntity<?> deleteUser(@RequestBody User user, String userName){
+//    	try {
+//    		user.setUserName(userName);
+//    		
+//    		userService.deleteUser(user);
+//    		
+//    		List<User> userList = userService.getUserList(userName);
+//    		
+//    		List<UserDTO> userDTOList = new ArrayList<UserDTO>();
+//    		
+//    		for(User t: userList) {
+//    			UserDTO userDTO = new UserDTO();
+//    			userDTO.setUserId(t.getUserId());
+//    			userDTO.setUserName(t.getUserName());
+//    			userDTO.setUserNickname(t.getUserNickname());
+//    			userDTO.setUserTel(t.getUserTel());
+//    			userDTO.setUserMail(t.getUserMail());
+//    			userDTO.setUserRole(t.getUserRole());
+//    			userDTO.setUserZip(t.getUserZip());
+//    			userDTO.setUserAddr(t.getUserAddr());
+//    			userDTO.setUserAddrDetail(t.getUserAddrDetail());
+//    			userDTO.setUserPoint(t.getUserPoint());
+//    			userDTO.setUserMarketing(t.getUserMarketing());
+//    			
+//    			userDTOList.add(userDTO);	
+//    		}
+//    		ResponseDTO<UserDTO> response = new ResponseDTO<>();
+//    		
+//    		response.setData(userDTOList);
+//    		
+//    		return ResponseEntity.ok().body(response);
+//    		
+//    	}catch(Exception e){
+//    		System.out.println(e.getMessage());
+//    		ResponseDTO<UserDTO> response = new ResponseDTO<>();
+//    		response.setError(e.getMessage());
+//    		return ResponseEntity.badRequest().body(response);		
+//    	}
+//    };
+//    
+//    
+//    //회원정보 수정
+//    @PutMapping("/updateAdminUser")
+//    public ResponseEntity<?> updateUser(@RequestBody User user, String userName){
+//    	try {
+//    		user.setUserId(userName);
+//    		
+//    		userService.deleteUser(user);
+//    		
+//    		List<User> userList = userService.getUserList(userName);
+//    		
+//    		List<UserDTO> userDTOList = new ArrayList<UserDTO>();
+//    		
+//    		for(User t: userList) {
+//    			UserDTO userDTO = new UserDTO();
+//    			userDTO.setUserId(t.getUserId());
+//    			userDTO.setUserName(t.getUserName());
+//    			userDTO.setUserNickname(t.getUserNickname());
+//    			userDTO.setUserTel(t.getUserTel());
+//    			userDTO.setUserMail(t.getUserMail());
+//    			userDTO.setUserRole(t.getUserRole());
+//    			userDTO.setUserZip(t.getUserZip());
+//    			userDTO.setUserAddr(t.getUserAddr());
+//    			userDTO.setUserAddrDetail(t.getUserAddrDetail());
+//    			userDTO.setUserPoint(t.getUserPoint());
+//    			userDTO.setUserMarketing(t.getUserMarketing());
+//    			
+//    			userDTOList.add(userDTO);	
+//    		}
+//    		ResponseDTO<UserDTO> response = new ResponseDTO<>();
+//    		
+//    		response.setData(userDTOList);
+//    		
+//    		return ResponseEntity.ok().body(response);
+//    		
+//    	}catch(Exception e){
+//    		System.out.println(e.getMessage());
+//    		ResponseDTO<UserDTO> response = new ResponseDTO<>();
+//    		response.setError(e.getMessage());
+//    		return ResponseEntity.badRequest().body(response);		
+//    	}
+//    };
 
-////회원 관리 
-//  //sc test
-//
-////	회원정보 조회
-//	ResponseEntity<?> getUserList() {
-//		return null;
-//	}
-//	ResponseEntity<?> getUser(User user) {
-//		return null;
-//	}
-//	
-////	회원정보 수정
-//	ResponseEntity<?> updateUser(User user) {
-//		return null;
-//	}
-//
-////	회원 삭제
-//	ResponseEntity<?> deleteUser(User user) {
-//		return null;
-//	}
+
 //
 //	
 ////주문 관리
--- src/main/java/com/bootreact/hmct/controller/user/UserController.java
+++ src/main/java/com/bootreact/hmct/controller/user/UserController.java
@@ -1,9 +1,16 @@
 package com.bootreact.hmct.controller.user;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -63,6 +70,129 @@ public class UserController {
 			return ResponseEntity.badRequest().body(response);
 		}
 	}
+	
+	//회원 정보 조회  
+    @GetMapping("/getUserList")
+    public ResponseEntity<?> getUserList(@AuthenticationPrincipal String userName){
+    	try {
+    		List<User> userList = userService.getUserList(userName);
+    		
+    		List<UserDTO> userDTOList = new ArrayList<UserDTO>();
+    		
+    		for(User t: userList) {
+    			UserDTO userDTO = new UserDTO();
+    			userDTO.setUserId(t.getUserId());
+    			userDTO.setUserName(t.getUserName());
+    			userDTO.setUserNickname(t.getUserNickname());
+    			userDTO.setUserTel(t.getUserTel());
+    			userDTO.setUserMail(t.getUserMail());
+    			userDTO.setUserRole(t.getUserRole());
+    			userDTO.setUserZip(t.getUserZip());
+    			userDTO.setUserAddr(t.getUserAddr());
+    			userDTO.setUserAddrDetail(t.getUserAddrDetail());
+    			userDTO.setUserPoint(t.getUserPoint());
+    			userDTO.setUserMarketing(t.getUserMarketing());
+    			
+    			userDTOList.add(userDTO);	
+    		}
+    		ResponseDTO<UserDTO> response = new ResponseDTO<>();
+    		
+    		response.setData(userDTOList);
+    		
+    		return ResponseEntity.ok().body(response);
+    		
+    	}catch(Exception e){
+    		System.out.println(e.getMessage());
+    		ResponseDTO<UserDTO> response = new ResponseDTO<>();
+    		response.setError(e.getMessage());
+    		return ResponseEntity.badRequest().body(response);		
+    	}
+    };
+    
+    //회원 삭제
+    @DeleteMapping("/deleteAdminUser")
+    public ResponseEntity<?> deleteUser(@RequestBody User user, String userName){
+    	try {
+    		user.setUserName(userName);
+    		
+    		userService.deleteUser(user);
+    		
+    		List<User> userList = userService.getUserList(userName);
+    		
+    		List<UserDTO> userDTOList = new ArrayList<UserDTO>();
+    		
+    		for(User t: userList) {
+    			UserDTO userDTO = new UserDTO();
+    			userDTO.setUserId(t.getUserId());
+    			userDTO.setUserName(t.getUserName());
+    			userDTO.setUserNickname(t.getUserNickname());
+    			userDTO.setUserTel(t.getUserTel());
+    			userDTO.setUserMail(t.getUserMail());
+    			userDTO.setUserRole(t.getUserRole());
+    			userDTO.setUserZip(t.getUserZip());
+    			userDTO.setUserAddr(t.getUserAddr());
+    			userDTO.setUserAddrDetail(t.getUserAddrDetail());
+    			userDTO.setUserPoint(t.getUserPoint());
+    			userDTO.setUserMarketing(t.getUserMarketing());
+    			
+    			userDTOList.add(userDTO);	
+    		}
+    		ResponseDTO<UserDTO> response = new ResponseDTO<>();
+    		
+    		response.setData(userDTOList);
+    		
+    		return ResponseEntity.ok().body(response);
+    		
+    	}catch(Exception e){
+    		System.out.println(e.getMessage());
+    		ResponseDTO<UserDTO> response = new ResponseDTO<>();
+    		response.setError(e.getMessage());
+    		return ResponseEntity.badRequest().body(response);		
+    	}
+    };
+    
+    
+    //회원정보 수정
+    @PutMapping("/updateAdminUser")
+    public ResponseEntity<?> updateUser(@RequestBody User user, String userName){
+    	try {
+    		user.setUserId(userName);
+    		
+    		userService.deleteUser(user);
+    		
+    		List<User> userList = userService.getUserList(userName);
+    		
+    		List<UserDTO> userDTOList = new ArrayList<UserDTO>();
+    		
+    		for(User t: userList) {
+    			UserDTO userDTO = new UserDTO();
+    			userDTO.setUserId(t.getUserId());
+    			userDTO.setUserName(t.getUserName());
+    			userDTO.setUserNickname(t.getUserNickname());
+    			userDTO.setUserTel(t.getUserTel());
+    			userDTO.setUserMail(t.getUserMail());
+    			userDTO.setUserRole(t.getUserRole());
+    			userDTO.setUserZip(t.getUserZip());
+    			userDTO.setUserAddr(t.getUserAddr());
+    			userDTO.setUserAddrDetail(t.getUserAddrDetail());
+    			userDTO.setUserPoint(t.getUserPoint());
+    			userDTO.setUserMarketing(t.getUserMarketing());
+    			
+    			userDTOList.add(userDTO);	
+    		}
+    		ResponseDTO<UserDTO> response = new ResponseDTO<>();
+    		
+    		response.setData(userDTOList);
+    		
+    		return ResponseEntity.ok().body(response);
+    		
+    	}catch(Exception e){
+    		System.out.println(e.getMessage());
+    		ResponseDTO<UserDTO> response = new ResponseDTO<>();
+    		response.setError(e.getMessage());
+    		return ResponseEntity.badRequest().body(response);		
+    	}
+    };
 	
 //	//휴대전화 인증
 //	void validateTel() {}
