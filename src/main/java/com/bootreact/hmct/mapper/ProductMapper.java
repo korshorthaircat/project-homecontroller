9d1b4c2064edb8d2a9309f542f68d974e58da8fb
saetbyeoloh <lily9999999@naver.com>
2022-08-30 12:07:38 +0900

    샛별_상품등록폼 완료_220830

---
+++ src/main/java/com/bootreact/hmct/mapper/ProductMapper.java
@@ -0,0 +1,10 @@
+package com.bootreact.hmct.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface ProductMapper {
+	@Select("SELECT IFNULL(MAX(PRODUCT_IMAGE_NO), 0) + 1 FROM T_HMCT_PRODUCT_IMAGE WHERE PRODUCT_NO = #{productNo}")
+	int getNextProductImageNo(int productNo);
+}
