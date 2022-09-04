1fb9a92dc2eb3d4472d243f8018a5e49312c9a7b
hoomzy <hoomzy@DESKTOP-BSPB1C9>
2022-09-05 00:26:35 +0900

    효주_main수정_220905

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -8,6 +8,8 @@ import EventCarousel from "./components/main/EventCarousel";
 import MainInteriorImage from "./components/productDetail/MainInteriorImage";
 import TopButton from "./components/main/TopButton";
 import WithScrollbar from "./components/main/Test";
+import  ScrollTop from "./components/main/ScrollTop";
+import Testt from "./components/main/Testt";
 
 
 
@@ -19,7 +21,7 @@ function App() {
       <MainImage/>
 
       
-      <h4 className="main_title">개발자들을 위한 인테리어 추천템!</h4>
+      <h4 className="main_title">개발자들을 위한 인테리어 추천 제품</h4>
       <MainInteriorImage/>
       <h4 className="main_title">진행중인 이벤트 및 프로모션</h4>
       <EventCarousel/>
@@ -29,9 +31,9 @@ function App() {
       <ServiceCarousel/>
       
       
-      <h4 className="main_title">기획전</h4>
+      <h4 className="main_title">따뜻한 분위기의 내추럴 우드인테리어</h4>
       <ProductCarousel/>
-      <h4 className="main_title">기획전</h4>
+      <h4 className="main_title">시들지 않는 조화로 매일 싱그럽게!</h4>
       <ProductCarousel/>
       <h4 className="main_title">인테리어 쇼룸</h4>
       <p className="sub_title">다양한 인테리어 제품을 한 눈에 볼 수 있는 인테리어쇼룸! 인테리어를 참고하세요.</p>
@@ -40,6 +42,7 @@ function App() {
       
       <TopButton />
       
+      {/* <Testt /> */}
       {/* <WithScrollbar /> */}
     </div>
 
--- src/main/frontend/src/components/main/MainShowroom.js
+++ src/main/frontend/src/components/main/MainShowroom.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MainShowroomColor from '../../components/main/MainShowroomColor';
 import "../../css/mainShowroom.css";
+import Button from "@mui/material/Button";
+import axios from 'axios';
+import ShowroomBox from './ShowroomBox';
+
+import data from './data.js';
+
 
 const MainShowroom = () => {
+  
+  
+
+  let [showroomImg, setShowroomImg] = useState(data);
+
+ 
     return (
-        <>
+        <div>
         <div className='mainShowroomColor'>
             <MainShowroomColor color="red" />
             <MainShowroomColor color="yellow" />
@@ -17,11 +29,61 @@ const MainShowroom = () => {
             <MainShowroomColor color="beige" />
             <MainShowroomColor color="gray" />    
         </div>
-        <div className='showroomBox'>
-        <div className='showroomBox_1'></div>
-        <div className='showroomBox_2'></div>
+
+
+       
+       
+        {/* <div className='showroomBox'> */}
+        {/* <div className='container'>
+          <div className='row'>
+        {showroomImg.map((test, i) => {
+          return <Testt
+          box={ShowroomBox[i]} i={i} key={i}
+          />
+        })}
         </div>
-        </>
+        </div> */}
+        {/* </div> */}
+
+        <div class="container text-center">
+          <div class="row row-cols-2">
+          
+          
+        {
+          showroomImg.map((a, i) => {
+            return (
+              <ShowroomBox showroomImg ={showroomImg[i]} />
+            )
+          })
+        }
+
+
+        </div>
+
+        </div>
+
+
+        <div className='mainShowroom_MoreBtn'>
+          <Button
+            variant="contained"
+            color="success"
+            sx={{ borderRadius: 12.5}}
+            onClick = {() => {
+              axios.get('')
+              .then((결과)=>{console.log(결과.data)
+                let copy = [...showroomImg, ...data.data]
+                setShowroomImg(copy)
+              })
+              .catch(()=> {
+                console.log('실패다')
+              })
+
+            }}
+          >
+            더 보기
+          </Button>
+       </div>
+        </div>
     );
 };
 
