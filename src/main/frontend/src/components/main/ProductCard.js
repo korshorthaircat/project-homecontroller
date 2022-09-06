875b8c47d52ffc184227494b428783a4b11c9c8b
hoomzy <hoomzy@DESKTOP-BSPB1C9>
2022-09-07 00:41:03 +0900

    효주_main product부분 수정_220907

--- src/main/frontend/src/components/main/ProductCard.js
+++ src/main/frontend/src/components/main/ProductCard.js
@@ -4,18 +4,77 @@ import IconButton from "@mui/material/IconButton";
 import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
 import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
 
-const ProductCard = ({ image, title, category, price }) => {
+function Component({item}){
+
+  switch(item.productCategory) {
+    case "C01" :
+      return "침대"
+    case "C02" :
+      return "쇼파"
+    case "C03" :
+      return "테이블/식탁/책상"
+    case "C04" :
+      return "거실장/tv장"
+    case "C05" :
+      return "서랍/수납장"
+    case "C06" : 
+      return "진열장/책장"
+    case "C07" :
+      return "선반"
+    case "C08" :
+      return "행거/옷장"
+    case "C09" :
+      return "의자"
+    case "C10" :
+      return "화장대/콘솔"
+    case "C11" :
+      return "조명"
+    case "C12" :
+      return "가전"
+    case "C13" :
+      return "패브릭"
+    case "C14" :
+      return "생필품"
+    case "C15" :
+      return "야외가구"
+    case "C16" :
+      return "주방용품"
+    default :
+      return "인테리어소품"
+  }
+}
+
+export {Component};
+
+
+const ProductCard = ({ item }) => {  
+
+
+    
+   
+
   return (
     <div className="card">
-      <img className="imageArea" src={image} alt="" />
+      <img className="imageArea" src="https://www.ikea.com/ext/ingkadam/m/61778d2a4441edd9/original/PH184645-crop001.jpg?f=xs" alt="사진" />
       <div className="textArea">
-        <p className="title_text">{title}</p>
-        <p className="category_text">{category}</p>
+        <p className="title_text">{item?.productName}</p>
+
+        
+
+        <p className="category_text">
+        {item?.productCategory}, {item?.productSize}
+
+
+
+
+
+            
+          </p>
         <div className="priceArea">
           <p>PRICE</p>
           <div className="last">
-            <p className="price_text">{price}</p>
-
+            <p className="price_text">\{item?.productPrice}</p>
+          
             <IconButton
               size="large"
               aria-label="show 17 new notifications"
--- src/main/java/com/bootreact/hmct/controller/main/MainController.java
+++ src/main/java/com/bootreact/hmct/controller/main/MainController.java
@@ -1,8 +1,26 @@
 package com.bootreact.hmct.controller.main;
 
-import org.springframework.http.ResponseEntity;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bootreact.hmct.dto.ProductDTO;
+import com.bootreact.hmct.dto.ResponseDTO;
+import com.bootreact.hmct.entity.Product;
+import com.bootreact.hmct.service.product.ProductService;
+
+
+@RestController
+@RequestMapping("/api/main")
 public class MainController {
+	
+	@Autowired
+	ProductService productService;
 	
 //	//헤더의 검색바 관련 메서드
 //	void getSearchProducts(String keyword) {}
@@ -13,9 +31,48 @@ public class MainController {
 //	}
 //
 //	//기획전 - 구체적 아이템(카테고리1)
-//	ResponseEntity<?> getProductList(String code) {
-//		return null;
-//	}
+	@GetMapping("/getProductList")
+	public ResponseEntity<?> getProductList() {
+		try {
+			List<Product> productList = productService.getProductList();
+		
+			List<ProductDTO> productDTOList = new ArrayList<ProductDTO>();
+		
+			for(Product p: productList) {
+				ProductDTO productDTO = new ProductDTO();
+				
+				productDTO.setProductNo(p.getProductNo());
+				productDTO.setProductName(p.getProductName());
+				productDTO.setProductState(p.getProductState());
+    			productDTO.setProductSize(p.getProductSize());
+    			productDTO.setProductRgsde(p.getProductRgsde());
+    			productDTO.setProductUpdde(p.getProductUpdde());
+				productDTO.setProductPrice(p.getProductPrice());
+				productDTO.setProductSummary(p.getProductSummary());
+    			productDTO.setProductDetail(p.getProductDetail());
+    			productDTO.setProductRef(p.getProductRef());
+    			productDTO.setProductMng(p.getProductMng());
+    			productDTO.setProductSafe(p.getProductSafe());
+    			productDTO.setProductDeliveryinfo(p.getProductDeliveryInfo());
+    			productDTO.setProductGauge(p.getProductGauge());
+    			productDTO.setProductMaterial(p.getProductMaterial());
+				productDTO.setProductCategory(p.getProductCategory());
+				
+				productDTOList.add(productDTO);
+			}
+			ResponseDTO<ProductDTO> response = new ResponseDTO<>();
+			
+			response.setData(productDTOList);
+			
+			return ResponseEntity.ok().body(response);
+			
+		} catch(Exception e) {
+			System.out.println(e.getMessage());
+			ResponseDTO<ProductDTO> response = new ResponseDTO<>();
+			response.setError(e.getMessage());
+			return ResponseEntity.badRequest().body(response);
+		}
+	};
 //
 //	//기획전 - 구체적 아이템(카테고리2)
 //
