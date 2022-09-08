b43110c15781720c1a76e33dbbc4bd60d5f3917c
saetbyeoloh <lily9999999@naver.com>
2022-09-08 10:43:28 +0900

    샛별_HashMap 추가 완료_220908

---
+++ src/main/java/com/bootreact/hmct/common/CamelHashMap.java
@@ -0,0 +1,15 @@
+package com.bootreact.hmct.common;
+
+import java.util.HashMap;
+
+import org.springframework.jdbc.support.JdbcUtils;
+
+@SuppressWarnings("serial")
+public class CamelHashMap extends HashMap<String, Object> {
+
+    @Override
+    public Object put(String key, Object value) {
+        return super.put(JdbcUtils.convertUnderscoreNameToPropertyName(key), value);
+    }
+    
+}
