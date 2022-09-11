516e4c940e579ddaae683ea9208e3df47ef04e78
iamjoy <iamjoy@218.38.137.28>
2022-09-11 20:51:50 +0900

    이기쁨_ full 받기위한 커밋_ 20220911

--- src/main/java/com/bootreact/hmct/service/wish/impl/WishServiceImpl.java
+++ src/main/java/com/bootreact/hmct/service/wish/impl/WishServiceImpl.java
@@ -1,33 +1,33 @@
-package com.bootreact.hmct.service.wish.impl;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import com.bootreact.hmct.mapper.WishItemMapper;
-import com.bootreact.hmct.mapper.WishShowroomMapper;
-import com.bootreact.hmct.service.wish.WishService;
-
-@Service
-public class WishServiceImpl implements WishService{
-
-	@Autowired
-	WishItemMapper wishitemMapper;
-	
-	@Autowired
-	WishShowroomMapper wishShowroomMapper;
-	
-	@Autowired
-	
-
-	@Override
-	public void addWish(String userId, int productNo) {
-		wishitemMapper.getWishItem(userId, productNo);
-		
-	}
-	
-	
-	
-}
+//package com.bootreact.hmct.service.wish.impl;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//import com.bootreact.hmct.mapper.WishItemMapper;
+//import com.bootreact.hmct.mapper.WishShowroomMapper;
+//import com.bootreact.hmct.service.wish.WishService;
+//
+//@Service
+//public class WishServiceImpl implements WishService{
+//
+//	@Autowired
+//	WishItemMapper wishitemMapper;
+//	
+//	@Autowired
+//	WishShowroomMapper wishShowroomMapper;
+//	
+//	@Autowired
+//	
+//
+//	@Override
+//	public void addWish(String userId, int productNo) {
+//		wishitemMapper.getWishItem(userId, productNo);
+//		
+//	}
+//	
+//	
+//	
+//}
