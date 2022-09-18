8016b646b83b8595705549f3fad4002ebeb53260
saetbyeoloh <lily9999999@naver.com>
2022-09-18 19:17:58 +0900

    샛별_리스트, 쇼룸 수정중 _ 2209018

--- src/main/frontend/src/components/showroom/InteriorShowroom.js
+++ src/main/frontend/src/components/showroom/InteriorShowroom.js
@@ -1,19 +1,90 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import Button from "@mui/material/Button";
 import React from "react";
-import { Container } from "@mui/system";
+import ShowroomTop from "../showroom/ShowroomTop";
+import Button from "@mui/material/Button";
+import axios from "axios";
+import { useState } from "react";
+import { useEffect } from "react";
+import ShowroomBox from "../main/ShowroomBox";
 import "../../css/showroom.css";
-import ShowroomColor from "./ShowroomColor";
 
-function InteriorShowroom() {
+const InteriorShowroom = () => {
+  const [showroomImg, setShowroomImg] = React.useState([]);
+
+  const [showroomImgData, setShowroomImgData] = useState([]);
+
+  const [cnt, setCnt] = useState(0);
+
+  let showroomListUrl = "http://localhost:8080/api/main/getShowroomList";
+
+  const list = () => {
+    axios
+      .get(showroomListUrl, {})
+      .then((response) => {
+        setShowroomImg(response.data.data);
+        setShowroomImgData(response.data.data.slice(0, 4));
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
+  React.useEffect(() => {
+    list();
+  }, []);
+
+  useEffect(() => {
+    console.log("sss", showroomImg);
+    console.log("ssss", showroomImg.slice(4 * cnt, 4 * (cnt + 1)));
+
+    let copy = showroomImgData.concat(
+      showroomImg.slice(4 * cnt, 4 * (cnt + 1))
+    );
+    console.log(copy);
+    setShowroomImgData(copy);
+  }, [cnt]);
+
+  const getColorShowroomList = (color) => {
+    console.log(color);
+    axios({
+      url: "http://localhost:8080/api/main/getColorShowroomList",
+      method: "get",
+      params: { showroomColor: color },
+    })
+      .then((response) => {
+        console.log(response.data.data);
+        setShowroomImg(response.data.data);
+        if (response.data.data.length > 4)
+          setShowroomImgData(response.data.data.slice(0, 4));
+        else setShowroomImgData(response.data.data);
+      })
+      .catch((e) => {});
+  };
+
   return (
-    <Container component="main" maxWidth="xl" style={{ marginTop: "3%" }}>
-      <ShowroomColor></ShowroomColor>
-      <div className="showroomMain"></div>
+    <div>
+      <div>
+        <ShowroomTop></ShowroomTop>
+        {showroomImgData.map((a) => (
+          <ShowroomBox item={a} />
+        ))}
+      </div>
 
-      <div className></div>
-    </Container>
+      <div className="mainShowroom_MoreBtn">
+        <Button
+          variant="contained"
+          color="success"
+          sx={{ borderRadius: 12.5 }}
+          onClick={() => {
+            setCnt(cnt + 1);
+            console.log("cnt", cnt);
+            console.log("data", showroomImgData);
+          }}
+        >
+          더 보기
+        </Button>
+      </div>
+    </div>
   );
-}
+};
 
 export default InteriorShowroom;
--- src/main/frontend/src/components/showroom/ShowroomColor.js
+++ src/main/frontend/src/components/showroom/ShowroomColor.js
@@ -1,13 +1,24 @@
 import React from "react";
-import "../../css/mainShowroom.css";
+import "../../css/showroomColorThumbnail.css";
 
-function ShowroomColor() {
+const ShowroomColor = (props) => {
   return (
     <div>
-      <h1 style={{ fontWeight: "bold" }}>인테리어 쇼룸</h1>
-      <p>나만의 color로 내 공간을 더 아름답게</p>
+      <img
+        className={"showroomColorThumbnail"}
+        src={props.imgChange}
+        alt=""
+        onClick={() => props.getColorShowroomList(props.color)}
+      />
+      <div className="showroombuttonbox">
+        <button
+          className={`showroomColor1 ${props.colorChange}`}
+          onClick={() => props.getColorShowroomList(props.color)}
+        />
+        <p>{props.colorname}</p>
+      </div>
     </div>
   );
-}
+};
 
 export default ShowroomColor;
