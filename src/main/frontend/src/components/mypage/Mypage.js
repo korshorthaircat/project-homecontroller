fde7d8744538d61f2f3edcc94c831a254a21167e
saetbyeoloh <lily9999999@naver.com>
2022-09-27 19:32:58 +0900

    샛별_220927_마이페지 수정

--- src/main/frontend/src/components/mypage/Mypage.js
+++ src/main/frontend/src/components/mypage/Mypage.js
@@ -14,79 +14,101 @@ function Mypage() {
     <div className="mypageTitle">
       <h1 className="My">마이페이지</h1>
       <div className="icons">
-        <Link href="/userupdate">
-          <img
-            className="a01"
-            src="../images/mypage_icons/user_update.png"
-            View
-            style={{
-              padding: 40,
-              flex: 1,
-              flexDirection: "row",
-            }}
-          />
-        </Link>
+        <div className="MypageTextbox">
+          <Link href="/userupdate">
+            <img
+              className="a01"
+              src="../images/mypage_icons/user_update.png"
+              View
+              style={{
+                padding: 40,
+                flex: 1,
+                flexDirection: "row",
+              }}
+            />
+          </Link>
+          <p>나의 정보</p>
+        </div>
 
-        <Link href="/wishlist">
-          <img
-            className="a02"
-            src="../images/mypage_icons/wishlist.png"
-            View
-            style={{
-              padding: 40,
-              flex: 1,
-              flexDirection: "row",
-            }}
-          />
-        </Link>
+        <div className="MypageTextbox">
+          <Link href="/wishlist">
+            <img
+              className="a02"
+              src="../images/mypage_icons/wishlist.png"
+              View
+              style={{
+                padding: 40,
+                flex: 1,
+                flexDirection: "row",
+              }}
+            />
+          </Link>
+          <p>위시리스트</p>
+        </div>
 
-        <Link href="/cart">
-          <img
-            className="a03"
-            src="../images/mypage_icons/cart.png"
-            View
-            style={{
-              padding: 40,
-              flex: 1,
-              flexDirection: "row",
-            }}
-          />
-        </Link>
-        <Link href="/orderlist">
-          <img
-            className="a04"
-            src="../images/mypage_icons/orderdetails.png"
-            View
-            style={{
-              padding: 40,
-              flex: 1,
-              flexDirection: "row",
-            }}
-          />
-        </Link>
-        <Link href="/myinquiry">
-          <img
-            className="a05"
-            src="../images/mypage_icons/board.png"
-            View
-            style={{
-              padding: 40,
-              flex: 1,
-              flexDirection: "row",
-            }}
-          />
-        </Link>
-        <Link href="/mypoint"></Link>
-        <img
-          className="a06"
-          src="../images/mypage_icons/cupon.png"
-          View
-          style={{
-            padding: 40,
-            // flex: 1,
-            flexDirection: "row",
-          }}
-        />
+        <div className="MypageTextbox">
+          <Link href="/cart">
+            <img
+              className="a03"
+              src="../images/mypage_icons/cart.png"
+              View
+              style={{
+                padding: 40,
+                flex: 1,
+                flexDirection: "row",
+              }}
+            />
+          </Link>
+          <p>장바구니</p>
+        </div>
+
+        <div className="MypageTextbox">
+          <Link href="/orderlist">
+            <img
+              className="a04"
+              src="../images/mypage_icons/orderdetails.png"
+              View
+              style={{
+                padding: 40,
+                flex: 1,
+                flexDirection: "row",
+              }}
+            />
+          </Link>
+          <p>주문내역</p>
+        </div>
+
+        <div className="MypageTextbox">
+          <Link href="/myinquiry">
+            <img
+              className="a05"
+              src="../images/mypage_icons/board.png"
+              View
+              style={{
+                padding: 40,
+                flex: 1,
+                flexDirection: "row",
+              }}
+            />
+          </Link>
+          <p>나의 게시글</p>
+        </div>
+
+        <div className="MypageTextbox">
+          <Link href="/mypoint">
+            <img
+              className="a06"
+              src="../images/mypage_icons/cupon.png"
+              View
+              style={{
+                padding: 40,
+                // flex: 1,
+                flexDirection: "row",
+              }}
+            />
+          </Link>
+          <p>포인트/쿠폰</p>
+        </div>
       </div>
     </div>
   );
